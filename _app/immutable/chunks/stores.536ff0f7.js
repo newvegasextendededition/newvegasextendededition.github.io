dd6e0ff705eb60599bc697d672e69ecf2fc1b78f
Alan <alan2008358@gmail.com>
2023-09-23 07:32:04 +0600

    Fix: install page alert style fix

--- _app/immutable/chunks/stores.536ff0f7.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.e099d1a1.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
